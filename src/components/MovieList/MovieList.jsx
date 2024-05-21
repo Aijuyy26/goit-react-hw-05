63f0a42d4be78ba169d369807fe4380753a5c401
Aijuyy26 <alexandralevchuk6@gmail.com>
2024-05-21 19:00:41 +0300

    ggg

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,21 +1,27 @@
-import {Link} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
 
-export default function MovieList ({movies, imageDetails}) {
-    return (
-        <div>
-            <ul>
-                {movies.map((movie) => {
-                    return (
-                        <li key={movie.id}>
-                            <Link to={`${movie.id}`}>
-                                <img src={movie.poster_path === null ? 'https://gdr.one/simg/185x278/d0e2ed/fff?text=poster' : `${imageDetails.baseUrl}${imageDetails.mediumSize}/${movie.poster_path}`} alt={movie.title} />
-                                <p>{movie.title}</p>
-                            </Link>
-                            
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+export default function MovieList({ movies, imageDetails }) {
+  const location = useLocation();
+
+  return (
+    <div>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link to={{ pathname: `${movie.id}`, state: { from: location } }}>
+              <img
+                src={
+                    movie.poster_path === null
+                      ? "https://gdr.one/simg/185x278/d0e2ed/fff?text=poster"
+                      : `${imageDetails.baseUrl}${imageDetails.mediumSize}/${movie.poster_path}`
+                  }
+                alt={movie.title}
+              />
+              <p>{movie.title}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
